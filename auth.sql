6e4a0686699772387a80759106f72750abb5f0c4
Lohit Krishna <31948001+lohit398@users.noreply.github.com>
2025-11-02 21:07:51 -0700

    Feat: Auth Controller

---
+++ auth.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users(
+    id serial PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role VARCHAR(5) NOT NULL
+);
+
+
+
---
+++ src/main/java/com/personal/development/cloud/Repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.personal.development.cloud.Repositories;
+
+import com.personal.development.cloud.Entities.User;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(String role);
+    Optional<User> findUserById(Long id); 
+}
